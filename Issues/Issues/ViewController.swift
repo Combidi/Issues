94386882518a747bb79bd4b41da666ea916c3ed0
Peter Combee <peter.combee@achmea.nl>
2022-08-22 19:29:22 +0200

    Add empty project

---
+++ Issues/Issues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Issues
+//
+//  Created by Peter Combee on 22/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
