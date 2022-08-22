ec512d7cfb81f576df2134b249d4577b84533064
Peter Combee <petercombee@gmail.com>
2022-08-22 19:58:29 +0200

    Declare `Issue` domain model

---
+++ Issues/Issues/Issue.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Peter Combee on 22/08/2022.
+//
+
+import Foundation
+
+struct Issue {
+    let firstName: String
+    let surname: String
+    let amountOfIssues: Int
+    let birthDate: Date
+}
