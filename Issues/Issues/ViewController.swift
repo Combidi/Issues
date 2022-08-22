ea24734797a2df4039dff094bf9819ec9131ed8e
Peter Combee <peter.combee@achmea.nl>
2022-08-22 19:41:00 +0200

    Cleanup

--- Issues/Issues/ViewController.swift
+++ Issues/Issues/ViewController.swift
@@ -1,19 +1,7 @@
 //
-//  ViewController.swift
-//  Issues
-//
 //  Created by Peter Combee on 22/08/2022.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+class ViewController: UIViewController {}
