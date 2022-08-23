ab3c6603b25a083fb54a5ab85c0e061108c7920b
Peter Combee <petercombee@gmail.com>
2022-08-23 21:40:41 +0200

    Move `IssuesPresenter` to its own file

--- Issues/Issues/Issues UI/IssuesViewController.swift
+++ Issues/Issues/Issues UI/IssuesViewController.swift
@@ -4,34 +4,6 @@
 
 import UIKit
 
-protocol IssuesView: AnyObject {
-    func present(issues: [Issue])
-    func presentLoading(_ isLoading: Bool)
-    func presentError(_ message: String?)
-}
-
-final class IssuesPresenter {
-    private let loader: IssuesLoader
-    
-    init(loader: IssuesLoader) {
-        self.loader = loader
-    }
-    
-    var view: IssuesView?
-    
-    func load() {
-        loader.loadIssues(completion: { [weak view] result in
-            switch result {
-            case let .success(issues):
-                view?.presentLoading(false)
-                view?.present(issues: issues)
-            case .failure:
-                view?.presentError("Invalid data")
-            }
-        })
-    }
-}
-
 public final class IssuesViewController: UITableViewController {
     
     private let presenter: IssuesPresenter
