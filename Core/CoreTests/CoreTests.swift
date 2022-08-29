554369e0dcfa444543fbb7d78434367636caf939
Peter Combee <petercombee@gmail.com>
2022-08-29 19:39:26 +0200

    Add empty Core project

---
+++ Core/CoreTests/CoreTests.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Peter Combee on 29/08/2022.
+//
+
+import XCTest
+@testable import Core
+
+class CoreTests: XCTestCase {
+
+    func test() {
+        XCTFail("Initial failing test")
+    }
+}
