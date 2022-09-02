3a49108da41499cd86512a7ce044e66f8af40893
Peter Combee <petercombee@gmail.com>
2022-09-02 08:19:14 +0200

    `LocalIssueLoader.loadIssues` delivers error on missing file

--- Core/CoreTests/Local Issues Loader/LocalIssueLoaderTests.swift
+++ Core/CoreTests/Local Issues Loader/LocalIssueLoaderTests.swift
@@ -26,6 +26,27 @@ final class LocalIssueLoaderTests: XCTestCase {
 
         XCTAssertEqual(mapperError as? NSError, anyError())
     }
+
+    func test_loadIssues_deliversErrorOnMissingFile() {
+        let fileURL = testSpecificFileURL()
+        let sut = LocalIssueLoader(fileURL: fileURL, mapper: { data in
+            throw anyError()
+        })
+
+        try! FileManager.default.removeItem(at: testSpecificFileURL())
+                
+        let exp = expectation(description: "wait for load completion")
+        var mapperError: Error?
+        sut.loadIssues {
+            if case let .failure(error) = $0 { mapperError = error }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+
+        XCTAssertEqual((mapperError as? NSError)?.code, fileNotFoundError().code)
+        XCTAssertEqual((mapperError as? NSError)?.domain, fileNotFoundError().domain)
+    }
     
     // MARK: Helpers
     
@@ -35,6 +56,10 @@ final class LocalIssueLoaderTests: XCTestCase {
     
     private func invalidData() -> Data {
         Data(capacity: 1)
+    }
+    
+    private func fileNotFoundError() -> NSError {
+        NSError(domain: "NSCocoaErrorDomain", code: 260)
     }
 }
 
