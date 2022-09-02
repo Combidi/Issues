e522431ca12e52edfd7fb083a8762b144bfd4b43
Peter Combee <petercombee@gmail.com>
2022-09-02 20:27:34 +0200

    `SceneDelegate.configureWindow` sets `UINavigation` controller with `IssuesViewController` as window rootViewController

--- Issues/Issues/SceneDelegate.swift
+++ Issues/Issues/SceneDelegate.swift
@@ -5,14 +5,18 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
+
+    private lazy var navigationController = UINavigationController(
+        rootViewController: IssuesViewController()
+    )
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
     
     func configureWindow() {
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 }
--- Issues/IssuesTests/SceneDelegateTests.swift
+++ Issues/IssuesTests/SceneDelegateTests.swift
@@ -16,6 +16,20 @@ final class SceneDelegateTests: XCTestCase {
 
         XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
     }
+    
+    func test_configureWindow_configuresRootViewController() {
+        let sut = SceneDelegate()
+        sut.window = UIWindow()
+        
+        sut.configureWindow()
+        
+        let root = sut.window?.rootViewController
+        let rootNavigation = root as? UINavigationController
+        let topController = rootNavigation?.topViewController
+        
+        XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead")
+        XCTAssertTrue(topController is IssuesViewController, "Expected a issues controller as top view controller, got \(String(describing: topController)) instead")
+    }
 }
 
 // MARK: - Helpers
