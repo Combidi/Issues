1b71b639aeb39992bc8c30585fab7b57a6225230
Peter Combee <petercombee@gmail.com>
2022-10-23 18:59:52 +0200

    .RemoteIssuesLoader.loadIssues` now uses completion blocks

--- Core/CoreTests/Issues API/RemoteIssuesLoaderTests.swift
+++ Core/CoreTests/Issues API/RemoteIssuesLoaderTests.swift
@@ -16,18 +16,25 @@ final class RemoteIssuesLoader {
         self.url = url
     }
     
-    func loadIssues() throws -> [Issue] {
-        let result = try client.get(from: url)
-        if result.1.statusCode != 200 {
-            throw InvalidDataError()
-        }
-        if result.0.isEmpty {
-            throw InvalidDataError()
-        } else {
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .iso8601
-            let issues = try decoder.decode(Issues.self, from: result.0)
-            return issues.toDomain()
+    func loadIssues(completion: @escaping (Result<[Issue], Error>) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+            case let .success((data, response)):
+                guard response.statusCode == 200, !data.isEmpty else {
+                    return completion(.failure(InvalidDataError()))
+                }
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+                    let issues = try decoder.decode(Issues.self, from: data)
+                    completion(.success(issues.toDomain()))
+                } catch {
+                    completion(.failure(error))
+                }
+
+            case let .failure(error):
+                completion(.failure(error))
+            }
         }
     }
         
@@ -63,17 +70,23 @@ final class RemoteIssuesLoader {
 }
 
 final class Client {
-    var loadedURLs = [URL]()
-    var stub: Result<(Data, HTTPURLResponse), Error> = .failure(NSError(domain: "any", code: 1))
-        
-    func get(from url: URL) throws -> (Data, HTTPURLResponse) {
-        loadedURLs.append(url)
-        switch stub {
-        case let .success(data):
-            return data
-        case let .failure(error):
-            throw error
-        }
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
+    private var messages = [(URL, (Result) -> Void)]()
+    private var completions: [(Result) -> Void] { messages.map(\.1) }
+    
+    var loadedURLs: [URL] { messages.map(\.0) }
+         
+    func get(from url: URL, completion: @escaping (Result) -> Void) {
+        messages.append((url, completion))
+    }
+    
+    func complete(with error: NSError, atIndex index: Int = 0) {
+        complete(with: .failure(error))
+    }
+    
+    func complete(with result: Result, atIndex index: Int = 0) {
+        completions[index](result)
     }
 }
 
@@ -83,7 +96,7 @@ class RemoteIssuesLoaderTests: XCTestCase {
         let url = URL(string: "https://a-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        _ = try? sut.loadIssues()
+        sut.loadIssues { _ in }
         
         XCTAssertEqual(client.loadedURLs, [url])
     }
@@ -92,55 +105,120 @@ class RemoteIssuesLoaderTests: XCTestCase {
         let url = URL(string: "https://a-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        _ = try? sut.loadIssues()
-        _ = try? sut.loadIssues()
+        sut.loadIssues { _ in }
+        sut.loadIssues { _ in }
         
         XCTAssertEqual(client.loadedURLs, [url, url])
     }
     
     func test_loadIssues_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        let clientError = NSError(domain: "a domain", code: 1)
-        client.stub = .failure(clientError)
             
-        XCTAssertThrowsError(try sut.loadIssues())
+        var receivedError: NSError?
+        let exp = expectation(description: "Wait for load completion")
+        sut.loadIssues { result in
+            if case let .failure(error) = result {
+                receivedError = error as NSError
+            }
+            exp.fulfill()
+        }
+        
+        let clientError = NSError(domain: "a domain", code: 1)
+        client.complete(with: clientError)
+
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(receivedError, clientError)
     }
         
     func test_loadIssues_deliverErrorOnEmptyData() {
         let (sut, client) = makeSUT()
+
+        var receivedError: NSError?
+        let exp = expectation(description: "Wait for load completion")
+        sut.loadIssues { result in
+            if case let .failure(error) = result {
+                receivedError = error as NSError
+            }
+            exp.fulfill()
+        }
+        
         let emptyData = Data()
-        client.stub = success(emptyData)
-    
-        XCTAssertThrowsError(try sut.loadIssues())
+        client.complete(with: success(emptyData))
+        
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertNotNil(receivedError)
     }
 
     func test_loadIssues_deliverEmptyListOnValidDataWithEmptyIssuesList() {
         let (sut, client) = makeSUT()
-        client.stub = success(emptyListData())
     
-        XCTAssertEqual(try sut.loadIssues(), [])
+        var receivedIssues: [Issue]?
+        let exp = expectation(description: "Wait for load completion")
+        sut.loadIssues { result in
+            if case let .success(issues) = result {
+                receivedIssues = issues
+            }
+            exp.fulfill()
+        }
+        
+        client.complete(with: success(emptyListData()))
+        
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(receivedIssues, [])
     }
     
     func test_loadIssues_deliversErrorOnInvalidJSON() {
         let (sut, client) = makeSUT()
         let invalidData = Data("non json data".utf8)
-        client.stub = success(invalidData)
 
-        XCTAssertThrowsError(try sut.loadIssues())
+        var receivedError: NSError?
+        let exp = expectation(description: "Wait for load completion")
+        sut.loadIssues { result in
+            if case let .failure(error) = result {
+                receivedError = error as NSError
+            }
+            exp.fulfill()
+        }
+        
+        client.complete(with: success(invalidData))
+        
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertNotNil(receivedError)
     }
     
-    func test_loadIssues_throwsOnNon200HTTPStatusCodeWithValidData() throws {
+    func test_loadIssues_deliversErrorOnNon200HTTPStatusCodeWithValidData() {
         let (sut, client) = makeSUT()
-        let samples = [199, 201, 300, 500]
         
-        try samples.forEach { statusCode in
-            client.stub = success(emptyListData(), statusCode: statusCode)
+        let samples = [199, 201, 300, 500]
+        samples.enumerated().forEach { index, statusCode in
             
-            XCTAssertThrowsError(try sut.loadIssues(), "for status code: \(statusCode)")
+            var receivedError: NSError?
+            let exp = expectation(description: "Wait for load completion")
+            sut.loadIssues { result in
+                if case let .failure(error) = result {
+                    receivedError = error as NSError
+                }
+                exp.fulfill()
+            }
+
+            client.complete(with: success(emptyListData(), statusCode: statusCode), atIndex: index)
+            
+            wait(for: [exp], timeout: 1.0)
+            XCTAssertNotNil(receivedError, "for status code: \(statusCode)")
         }
     }
     
     func test_loadIssues_deliversIssuesOnValidNonEmptyIssuesList() {
+        let (sut, client) = makeSUT()
+        
+        var receivedIssues: [Issue]?
+        let exp = expectation(description: "Wait for load completion")
+        sut.loadIssues { result in
+            if case let .success(issues) = result {
+                receivedIssues = issues
+            }
+            exp.fulfill()
+        }
         
         let validNonEmptyIssuesData = Data("""
         {
@@ -168,16 +246,15 @@ class RemoteIssuesLoaderTests: XCTestCase {
             ]
         }
         """.utf8)
-        
+
+        client.complete(with: success(validNonEmptyIssuesData))
+
+        wait(for: [exp], timeout: 1.0)
         let expected = [
             Issue(firstName: "Peter", surname: "Combee", submissionDate: Date(timeIntervalSince1970: 1598627222), subject: "A subject"),
             Issue(firstName: "Luna", surname: "Combee", submissionDate: Date(timeIntervalSince1970: 1577881882), subject: "Another subject"),
         ]
-        
-        let (sut, client) = makeSUT()
-        client.stub = success(validNonEmptyIssuesData)
-        
-        XCTAssertEqual(try sut.loadIssues(), expected)
+        XCTAssertEqual(receivedIssues, expected)
     }
 
     // MARK: Helpers
