16d5045ea7533feac8ddb054ceafe4bc8ddd7460
Peter Combee <petercombee@gmail.com>
2022-10-23 19:29:41 +0200

    Move mapping related decodable types out of `IssuesMapper`

--- Core/Core/Issues API/IssuesMapper.swift
+++ Core/Core/Issues API/IssuesMapper.swift
@@ -5,39 +5,39 @@
 import Foundation
 
 enum IssuesMapper {
-    private struct Issues: Decodable {
-        struct Customer: Decodable {
-            let first_name: String
-            let last_name: String
-        }
-        
-        struct Message: Decodable {
-            let subject: String
-        }
-        
-        struct Item: Decodable {
-            let customer: Customer
-            let created_at: Date
-            let message: Message
-        }
-        
-        let issues: [Item]
-        
-        func toDomain() -> [Issue] {
-            issues.map {
-                Issue(
-                    firstName: $0.customer.first_name,
-                    surname: $0.customer.last_name,
-                    submissionDate: $0.created_at,
-                    subject: $0.message.subject
-                )
-            }
-        }
-    }
-
     static func map(data: Data, response: HTTPURLResponse) throws -> [Issue] {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601
         return try decoder.decode(Issues.self, from: data).toDomain()
+    }
+}
+
+private struct Issues: Decodable {
+    struct Customer: Decodable {
+        let first_name: String
+        let last_name: String
+    }
+
+    struct Message: Decodable {
+        let subject: String
+    }
+
+    struct Item: Decodable {
+        let customer: Customer
+        let created_at: Date
+        let message: Message
+    }
+    
+    let issues: [Item]
+    
+    func toDomain() -> [Issue] {
+        issues.map {
+            Issue(
+                firstName: $0.customer.first_name,
+                surname: $0.customer.last_name,
+                submissionDate: $0.created_at,
+                subject: $0.message.subject
+            )
+        }
     }
 }
