b4ffe0e35610ee6fd20eced37693367441a36689
Peter Combee <petercombee@gmail.com>
2022-10-23 19:22:16 +0200

    Move `RemoteIssuesLoader` to its own file in production

---
+++ Core/Core/Issues API/RemoteIssuesLoader.swift
@@ -0,0 +1,68 @@
+//
+//  Created by Peter Combee on 23/10/2022.
+//
+
+import Foundation
+
+public final class RemoteIssuesLoader {
+    struct InvalidDataError: Swift.Error {}
+        
+    private let client: HTTPClient
+    private let url: URL
+    
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func loadIssues(completion: @escaping (Result<[Issue], Error>) -> Void) {
+        client.get(from: url) { result in
+            switch result {
+            case let .success((data, response)):
+                guard response.statusCode == 200, !data.isEmpty else {
+                    return completion(.failure(InvalidDataError()))
+                }
+                completion(Result { try Self.map(data: data, response: response) })
+
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    private static func map(data: Data, response: HTTPURLResponse) throws -> [Issue] {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return try decoder.decode(Issues.self, from: data).toDomain()
+    }
+    
+    private struct Issues: Decodable {
+        struct Customer: Decodable {
+            let first_name: String
+            let last_name: String
+        }
+        
+        struct Message: Decodable {
+            let subject: String
+        }
+        
+        struct Item: Decodable {
+            let customer: Customer
+            let created_at: Date
+            let message: Message
+        }
+        
+        let issues: [Item]
+        
+        func toDomain() -> [Issue] {
+            issues.map {
+                Issue(
+                    firstName: $0.customer.first_name,
+                    surname: $0.customer.last_name,
+                    submissionDate: $0.created_at,
+                    subject: $0.message.subject
+                )
+            }
+        }
+    }
+}
--- Core/CoreTests/Issues API/RemoteIssuesLoaderTests.swift
+++ Core/CoreTests/Issues API/RemoteIssuesLoaderTests.swift
@@ -5,69 +5,6 @@
 import XCTest
 import Core
 
-final class RemoteIssuesLoader {
-    struct InvalidDataError: Swift.Error {}
-        
-    private let client: HTTPClient
-    private let url: URL
-    
-    init(client: HTTPClient, url: URL) {
-        self.client = client
-        self.url = url
-    }
-    
-    func loadIssues(completion: @escaping (Result<[Issue], Error>) -> Void) {
-        client.get(from: url) { result in
-            switch result {
-            case let .success((data, response)):
-                guard response.statusCode == 200, !data.isEmpty else {
-                    return completion(.failure(InvalidDataError()))
-                }
-                completion(Result { try Self.map(data: data, response: response) })
-
-            case let .failure(error):
-                completion(.failure(error))
-            }
-        }
-    }
-
-    private static func map(data: Data, response: HTTPURLResponse) throws -> [Issue] {
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .iso8601
-        return try decoder.decode(Issues.self, from: data).toDomain()
-    }
-    
-    private struct Issues: Decodable {
-        struct Customer: Decodable {
-            let first_name: String
-            let last_name: String
-        }
-        
-        struct Message: Decodable {
-            let subject: String
-        }
-        
-        struct Item: Decodable {
-            let customer: Customer
-            let created_at: Date
-            let message: Message
-        }
-        
-        let issues: [Item]
-        
-        func toDomain() -> [Issue] {
-            issues.map {
-                Issue(
-                    firstName: $0.customer.first_name,
-                    surname: $0.customer.last_name,
-                    submissionDate: $0.created_at,
-                    subject: $0.message.subject
-                )
-            }
-        }
-    }
-}
-
 final class ClientSpy: HTTPClient {
     typealias Result = HTTPClient.Result
     
