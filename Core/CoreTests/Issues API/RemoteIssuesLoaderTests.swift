e0fe978cb9fc590485ee73ba5c0d9dc71385c760
Peter Combee <petercombee@gmail.com>
2022-10-23 12:20:04 +0200

    `RemoteIssuesLoader.loadIssues` delivers issues on valid non-empty issues data

--- Core/CoreTests/Issues API/RemoteIssuesLoaderTests.swift
+++ Core/CoreTests/Issues API/RemoteIssuesLoaderTests.swift
@@ -7,9 +7,7 @@ import Core
 
 final class RemoteIssuesLoader {
     struct InvalidDataError: Swift.Error {}
-    
-    private struct Item: Decodable {}
-    
+        
     private let client: Client
     private let url: URL
     
@@ -23,10 +21,39 @@ final class RemoteIssuesLoader {
         if result.isEmpty {
             throw InvalidDataError()
         } else {
-            if let henk = try? JSONDecoder().decode(Item.self, from: result) {
-                return []
-            } else {
-                throw InvalidDataError()
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            let issues = try decoder.decode(Issues.self, from: result)
+            return issues.toDomain()
+        }
+    }
+        
+    private struct Issues: Decodable {
+        struct Customer: Decodable {
+            let name: String
+        }
+        
+        struct Message: Decodable {
+            let subject: String
+        }
+        
+        struct Item: Decodable {
+            let customer: Customer
+            let created_at: Date
+            let message: Message
+        }
+        
+        let issues: [Item]
+        
+        func toDomain() -> [Issue] {
+            issues.map {
+                let nameComponents = $0.customer.name.components(separatedBy: .whitespaces)
+                return Issue(
+                    firstName: nameComponents.first!,
+                    surname: nameComponents.last!,
+                    submissionDate: $0.created_at,
+                    subject: $0.message.subject
+                )
             }
         }
     }
@@ -72,7 +99,7 @@ class RemoteIssuesLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         let clientError = NSError(domain: "any", code: 1)
         client.stub = .failure(clientError)
-    
+            
         XCTAssertThrowsError(try sut.loadIssues())
     }
     
@@ -102,6 +129,44 @@ class RemoteIssuesLoaderTests: XCTestCase {
         client.stub = .success(invalidData)
 
         XCTAssertThrowsError(try sut.loadIssues())
+    }
+    
+    func test_loadIssues_deliversIssuesOnValidNonEmptyIssuesList() {
+        
+        let validNonEmptyIssuesData = Data("""
+        {
+            "issues": [
+                {
+                    "customer": {
+                        "name": "Peter Combee"
+                    },
+                    "created_at": "2020-08-28T15:07:02+00:00",
+                    "message": {
+                        "subject": "A subject"
+                    }
+                },
+                {
+                    "customer": {
+                        "name": "Luna Combee"
+                    },
+                    "created_at": "2020-01-01T12:31:22+00:00",
+                    "message": {
+                        "subject": "Another subject"
+                    }
+                }
+            ]
+        }
+        """.utf8)
+        
+        let expected = [
+            Issue(firstName: "Peter", surname: "Combee", submissionDate: Date(timeIntervalSince1970: 1598627222), subject: "A subject"),
+            Issue(firstName: "Luna", surname: "Combee", submissionDate: Date(timeIntervalSince1970: 1577881882), subject: "Another subject"),
+        ]
+        
+        let (sut, client) = makeSUT()
+        client.stub = .success(validNonEmptyIssuesData)
+        
+        XCTAssertEqual(try sut.loadIssues(), expected)
     }
     
     // MARK: Helpers
