b3e858e00873e0e9473a8bf128998702317f39a5
Peter Combee <petercombee@gmail.com>
2023-02-10 10:42:29 +0100

    Move `IssuesLoadingView` to its own file

---
+++ Core/Core/Issues Presentation/IssuesLoadingView.swift
@@ -0,0 +1,7 @@
+//
+//  Created by Peter Combee on 10/02/2023.
+//
+
+public protocol IssuesLoadingView {
+    func presentLoading(_ flag: Bool)
+}
--- Core/Core/Issues Presentation/IssuesView.swift
+++ Core/Core/Issues Presentation/IssuesView.swift
@@ -2,10 +2,6 @@
 //  Created by Peter Combee on 29/08/2022.
 //
 
-public protocol IssuesLoadingView {
-    func presentLoading(_ flag: Bool)
-}
-
 public protocol IssuesView {
     func present(issues: [IssueViewModel])
 }
