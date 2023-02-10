4d79e429b437850df955b755c7eb4506f91dfbc6
Peter Combee <petercombee@gmail.com>
2023-02-10 10:41:54 +0100

    Move `IssuesErrorView` to its own file

--- Core/Core/Issues Presentation/IssuesView.swift
+++ Core/Core/Issues Presentation/IssuesView.swift
@@ -6,10 +6,6 @@ public protocol IssuesLoadingView {
     func presentLoading(_ flag: Bool)
 }
 
-public protocol IssuesErrorView {
-    func presentMessage(_ message: String?)    
-}
-
 public protocol IssuesView {
     func present(issues: [IssueViewModel])
 }
