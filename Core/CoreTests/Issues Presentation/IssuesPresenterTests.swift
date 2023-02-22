9381799e379ec1763c9bcf78283ff74b55142d33
Peter Combee <petercombee@gmail.com>
2023-02-22 14:03:54 +0100

    Remove duplicate test helpers

--- Core/CoreTests/Issues Presentation/IssuesPresenterTests.swift
+++ Core/CoreTests/Issues Presentation/IssuesPresenterTests.swift
@@ -143,12 +143,3 @@ private final class ViewSpy: IssuesView, LoadingView, ErrorView {
         capturedLoadings.append(isLoading)
     }
 }
-
-
-private extension XCTestCase {
-    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been delocated. Potential memory leak", file: file, line: line)
-        }
-    }
-}
