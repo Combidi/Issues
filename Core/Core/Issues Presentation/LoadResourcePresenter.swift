beed65866aab2724463c8b47ffe352edff07e3bf
Peter Combee <petercombee@gmail.com>
2023-02-22 20:13:42 +0100

    Move `ResourceLoadingErrorViewModel` to its own file

--- Core/Core/Issues Presentation/LoadResourcePresenter.swift
+++ Core/Core/Issues Presentation/LoadResourcePresenter.swift
@@ -2,10 +2,6 @@
 //  Created by Peter Combee on 22/02/2023.
 //
 
-public struct ResourceLoadingErrorViewModel {
-    public let message: String?
-}
-
 public protocol ResourceLoadingView {
     func display(_ viewModel: ResourceLoadingViewModel)
 }
