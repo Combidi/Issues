7dd0099391e5980e2fb2fc49ec0dc713610bf42d
Peter Combee <petercombee@gmail.com>
2023-02-22 14:03:16 +0100

    Move memory leak tracking helpers to shared scope

--- Core/CoreTests/Issues API/URLSessionHTTPClientTests.swift
+++ Core/CoreTests/Issues API/URLSessionHTTPClientTests.swift
@@ -199,11 +199,3 @@ private class URLProtocolStub: URLProtocol {
     
     override func stopLoading() {}
 }
-
-private extension XCTestCase {
-    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak", file: file, line: line)
-        }
-    }
-}
