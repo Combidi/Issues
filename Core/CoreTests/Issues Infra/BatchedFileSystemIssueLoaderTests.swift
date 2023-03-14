d33f3dd0da326e10fb3f8901c7a446036e89f0ac
Peter Combee <petercombee@gmail.com>
2023-03-14 16:51:39 +0100

    `BatchedFileSystemIssueLoader.loadIssues` delivers error on mapping error

--- Core/CoreTests/Issues Infra/BatchedFileSystemIssueLoaderTests.swift
+++ Core/CoreTests/Issues Infra/BatchedFileSystemIssueLoaderTests.swift
@@ -5,6 +5,11 @@
 import XCTest
 
 final class StreamingReader {
+    
+    init(stub: [String] = []) {
+        
+    }
+    
     private(set) var nextLineCallCount = 0
 }
 
@@ -12,15 +17,31 @@ final class BatchedFileSystemIssueLoader {
     init(streamingReader: StreamingReader) {
         
     }
+    
+    func loadIssues(completion: (Error) -> Void) {
+        completion(NSError(domain: "any", code: 0))
+    }
 }
 
 class BatchedFileSystemIssueLoaderTests: XCTestCase {
- 
+     
     func test_doesNotRequestLinesOnInit() {
         
         let streamingReader = StreamingReader()
         let _ = BatchedFileSystemIssueLoader(streamingReader: streamingReader)
                 
         XCTAssertEqual(streamingReader.nextLineCallCount, 0)
+    }
+    
+    func test_loadIssues_deliversErrorOnMappingError() {
+        let streamingReader = StreamingReader(stub: [
+            "invalid data"
+        ])
+        let sut = BatchedFileSystemIssueLoader(streamingReader: streamingReader)
+
+        var receivedError: Error?
+        sut.loadIssues { receivedError = $0 }
+        
+        XCTAssertNotNil(receivedError)
     }
 }
