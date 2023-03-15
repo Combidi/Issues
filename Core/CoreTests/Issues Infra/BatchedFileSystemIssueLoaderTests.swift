52a19c453862adfe6edc66c931b17e1871c00ce5
Peter Combee <petercombee@gmail.com>
2023-03-15 08:28:13 +0100

    Remove redundant tests

--- Core/CoreTests/Issues Infra/BatchedFileSystemIssueLoaderTests.swift
+++ Core/CoreTests/Issues Infra/BatchedFileSystemIssueLoaderTests.swift
@@ -11,11 +11,8 @@ final class StreamingReader {
     init(stub: [String]?) {
         self.stub = stub?.reversed()
     }
-    
-    private(set) var nextLineCallCount = 0
-    
+        
     func nextLine() -> String? {
-        nextLineCallCount += 1
         return stub?.popLast()
     }
 }
@@ -23,16 +20,18 @@ final class BatchedFileSystemIssueLoader {
     typealias Mapper = (String) throws -> Issue
     
     private let streamingReader: StreamingReader
+    private let batchSize: Int
     private let mapper: Mapper
     
-    init(streamingReader: StreamingReader, mapper: @escaping Mapper) {
+    init(streamingReader: StreamingReader, batchSize: Int, mapper: @escaping Mapper) {
         self.streamingReader = streamingReader
+        self.batchSize = batchSize
         self.mapper = mapper
     }
     
     func loadIssues(completion: (Result<[Issue], Error>) -> Void) {
         var issues = [Issue]()
-        for _ in 0..<5 {
+        for _ in 0..<batchSize {
             guard let nextLine = streamingReader.nextLine() else {
                 return completion(.success(issues))
             }
@@ -48,21 +47,7 @@ final class BatchedFileSystemIssueLoader {
 }
 
 class BatchedFileSystemIssueLoaderTests: XCTestCase {
-     
-    func test_doesNotRequestLinesOnInit() {
-        let (_, streamingReader) = makeSUT()
-        
-        XCTAssertEqual(streamingReader.nextLineCallCount, 0)
-    }
-    
-    func test_loadIssues_requestsNextLine() {
-        let (sut, streamingReader) = makeSUT()
-
-        sut.loadIssues { _ in }
-        
-        XCTAssertEqual(streamingReader.nextLineCallCount, 1)
-    }
-    
+         
     func test_loadIssues_deliversErrorOnMappingError() {
         let mapperError = NSError(domain: "any", code: 1)
         let (sut, _) = makeSUT(
@@ -101,16 +86,38 @@ class BatchedFileSystemIssueLoaderTests: XCTestCase {
         XCTAssertEqual(receivedIssues, expectedIssues)
     }
     
+    func test_loadIssues_doesNotDeliverMoreThanFixedAmountOfIssues() {
+        let (sut, _) = makeSUT(
+            batchSize: 3,
+            readerStub: ["Peter", "Henk", "Kees", "Klaas"],
+            mapper: makeIssue
+        )
+
+        var receivedIssues: [Issue]?
+        sut.loadIssues { result in
+            if case let .success(issues) = result {
+                receivedIssues = issues
+            }
+        }
+        
+        XCTAssertEqual(receivedIssues?.count, 3)
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(
+        batchSize: Int = 5,
         readerStub: [String] = [],
         mapper: @escaping BatchedFileSystemIssueLoader.Mapper = { _ in anyIssue() },
         file: StaticString = #filePath,
         line: UInt = #line
     ) -> (BatchedFileSystemIssueLoader, StreamingReader) {
         let streamingReader = StreamingReader(stub: readerStub)
-        let sut = BatchedFileSystemIssueLoader(streamingReader: streamingReader, mapper: mapper)
+        let sut = BatchedFileSystemIssueLoader(
+            streamingReader: streamingReader,
+            batchSize: batchSize,
+            mapper: mapper
+        )
         
         trackForMemoryLeaks(streamingReader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
