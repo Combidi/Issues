ecbc0ef19d081814d9f796752f4457d24dc22e24
Peter Combee <petercombee@gmail.com>
2023-05-10 15:33:55 +0200

    Localize resource loading error message

--- Core/CoreTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ Core/CoreTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -9,13 +9,13 @@ final class LoadResourcePresenterTests: XCTestCase {
     
     func test_doesNotSendMessagesOnCreation() {
         let (_, view) = makeSUT()
-
+        
         XCTAssertEqual(view.messages, [])
     }
     
     func test_didStartLoading_displaysLoadingAndHidesError() {
         let (sut, view) = makeSUT()
-
+        
         sut.didStartLoading()
         
         XCTAssertEqual(view.messages, [
@@ -31,13 +31,13 @@ final class LoadResourcePresenterTests: XCTestCase {
         
         XCTAssertEqual(view.messages, [
             .display(isLoading: false),
-            .display(errorMessage: "Invalid data")
+            .display(errorMessage: localized("LOAD_RESOURCE_ERROR"))
         ])
     }
     
     func test_didFinishLoading_displaysDisplaysMappedResourceAndStopsLoading() {
         let (sut, view) = makeSUT(mapper: { resource in return resource.uppercased() })
-
+        
         sut.didFinishLoading(with: "resource")
         
         XCTAssertEqual(view.messages, [
@@ -45,7 +45,7 @@ final class LoadResourcePresenterTests: XCTestCase {
             .display(resourceViewModel: "RESOURCE")
         ])
     }
-
+    
     // MARK: Helpers
     
     private typealias SUT = LoadResourcePresenter<String, ViewSpy>
@@ -60,6 +60,16 @@ final class LoadResourcePresenterTests: XCTestCase {
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
+    }
+    
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Shared"
+        let bundle = Bundle(for: SUT.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
     }
 }
 
